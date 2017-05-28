9a451af8d67477fee2ea83bf3fb559ad4789fe88
Kiyotaka Saito <kiyotaka-saitou@united.jp>
2017-05-28 23:11:59 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Item;
 
 class ItemsController extends Controller
 {
@@ -74,7 +75,13 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-        //
+        $item = Item::find($id);
+        $want_users = $item->want_users;
+
+        return view('items.show', [
+            'item' => $item,
+            'want_users' => $want_users,
+        ]);
     }
 
     /**
